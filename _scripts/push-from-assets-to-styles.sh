90f3cdc474da1eec6ad369fe7ccb45103fbfb2c1
djacquel <github@pygmeeweb.com>
2015-12-07 20:47:15 +0100

    trying another bash

--- _scripts/push-from-assets-to-styles.sh
+++ _scripts/push-from-assets-to-styles.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # The -e flag causes the script to exit as soon as one command returns a non-zero exit code
 # The -v flag makes the shell print all lines in the script before executing them, which helps identify which steps failed.
