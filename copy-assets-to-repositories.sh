b2024b988d0a50a5ab8005fc6790726a26db8847
djacquel <github@pygmeeweb.com>
2015-12-11 16:10:00 +0100

    script

--- copy-assets-to-repositories.sh
+++ copy-assets-to-repositories.sh
@@ -2,54 +2,69 @@
 ##############################################
 ##   ASSETS BUILD AND REPLICATION SCRIPT
 ##   FOR LOCAL USE
+##   RUN FROM "ASSETS" FOLDER
 ##
 ##        ++ USE WITH CAUTION !! ++
 ##
 ##############################################
 
-#bundle exec jekyll build
+#echo -n "With push? ('y' will push other no) [ENTER]: "
+#read doPush
+
+bundle exec jekyll build
 
 DIR="$( pwd )"
 echo "Working from ${DIR}"
 ORIGIN="${DIR}/_site/assets"
 echo "Generated files at ${ORIGIN}"
 
-STYLES="${DIR}/../styles"
-STYLESFOLDER="/assets"
-# REPOS=("website" "datacarpentry.github.io" "lesson-template" "workshop-template")
-REPOS=("../website")
-REPOSFOLDER="assets-fallback"
+STYLES="/../styles"
+STYLESFOLDER="assets"
+
+#REPOS=("/../website" "/../datacarpentry.github.io" "/../lesson-template" "/../workshop-template")
+REPOS=("/../workshop-template")
+
+ASSETSFOLDER="assets-fallback"
 
 function copyTo {
-    echo -e "\n[copyTo]"
-    echo "delete existing files in ${1}"
-    rm -rf $1/*
-    echo "copy from $ORIGIN to ${1}"
-    test -d "$1" || mkdir -p "$1"
-    cp -R ${ORIGIN}/* $1
-    doNotEditFilePath="${1}/_DO_NOT_EDIT_THIS_FOLDER"
-    echo -e "Creating doNotEdit File at ${doNotEditFilePath}\n"
+    echo "[copyTo]"
+    echo "Parameter #1 is $1 - Parameter #2 is $2 - Parameter #3 is $3"
+    cd $DIR
+    echo "currently in $(pwd)"
+    targetRepo="${DIR}${1}"
+    assetsDestination="${targetRepo}/${2}"
+    echo "deleting existing files from ${assetsDestination}"
+    rm -rf $assetsDestination/*
+    echo "copy from $ORIGIN to ${assetsDestination}"
+    test -d "$assetsDestination" || mkdir -p "$assetsDestination"
+    cp -R ${ORIGIN}/* $assetsDestination
+    doNotEditFilePath="${assetsDestination}/_DO_NOT_EDIT_THIS_FOLDER"
+    echo "Creating doNotEdit File at ${doNotEditFilePath}"
     echo '' > ${doNotEditFilePath}
-    echo -e "\n${1} now contains"
-    tree -R $1
-    echo -e "-------------"
+    echo "${assetsDestination} now contains"
+#    tree -R $assetsDestination
+    echo -e " -- Committing results from ${targetRepo}"
+    cd $targetRepo
+    echo "currently in $(pwd)"
+
+    if [ -z $(git status --porcelain) ]
+        then
+            echo "Nothing to commit"
+        else
+            echo "Commiting"
+            git add -fA "./$2"
+            git commit -m "Automatic commit after local update"
+
+            echo "Pushing"
+            git pull origin gh-pages
+            git push -fq origin gh-pages
+    fi
+    echo -e "-------------\n"
 }
 
-stylesAssetsPath="${STYLES}${STYLESFOLDER}"
-copyTo $stylesAssetsPath
+copyTo $STYLES $STYLESFOLDER "true"
 
 echo -e "Starting to copy in repositories"
 for repo in ${REPOS[@]}; do
-    repoPath="${DIR}/${repo}/${REPOSFOLDER}"
-    copyTo $repoPath
+    copyTo $repo $ASSETSFOLDER "false"
 done
-
-#git add -f --ignore-removal .
-#git commit -m "Automatic commit after local update"
-#git push -fq origin gh-pages
-
-cd ../assets
-
-git add -f --ignore-removal .
-git commit -m "Automatic commit after local update"
-git push -fq origin gh-pages
